bf5da3db0184cbd95afbc501e3519e11a70d6355
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2018-08-17 18:42:01 +0100

    Fix the test

--- src/test/scala/kamon/cloudwatch/CloudWatchReporterSpec.scala
+++ src/test/scala/kamon/cloudwatch/CloudWatchReporterSpec.scala
@@ -6,36 +6,27 @@ import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.{givenThat => _, _}
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.junit.Stubbing
-import com.github.tomakehurst.wiremock.matching.{ContentPattern, RegexPattern}
+import com.github.tomakehurst.wiremock.matching.{StringValuePattern, MatchResult}
 import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection.JavaConverters._
 
 object CloudWatchReporterSpec {
+  final val TestClock: Clock = Clock.fixed(Instant.EPOCH, ZoneOffset.UTC)
+
   final val TestConfig = ConfigFactory.parseString(
     """kamon.cloudwatch {
-      |  # namespace is the AWS Metrics custom namespace
-      |  namespace = kamon-cloudwatch
-      |
-      |  # AWS region, on ec2 region is fetched by getCurrentRegion command
-      |  region = eu-west-1
-      |
-      |  # batch size of data when send to Cloudwatch
+      |  namespace = kamon-cloudwatch-test
       |  batch-size = 20
-      |
-      |  # only logs metrics to file without shipping out to Cloudwatch if it is false
       |  send-metrics = true
-      |
-      |  # how many threads will be assigned to the pool that does the shipment of metrics
       |  async-threads = 5
       |}""".stripMargin
   )
 }
 
 class CloudWatchReporterSpec extends FlatSpec with Matchers {
-
-  val TestClock: Clock = Clock.fixed(Instant.EPOCH, ZoneOffset.UTC)
+  import CloudWatchReporterSpec._
 
   def withCloudWatch(config: Config = ConfigFactory.load())(testCode: (Stubbing, CloudWatchReporter) => Any): Unit = {
     val fixture = new Fixture(config)
@@ -47,21 +38,19 @@ class CloudWatchReporterSpec extends FlatSpec with Matchers {
     }
   }
 
-  "the reporter" should "do something" in withCloudWatch() { (stub, reporter) =>
+  "the reporter" should "publish metrics" in withCloudWatch(TestConfig) { (stub, reporter) =>
     val snapshot = new PeriodSnapshotBuilder()
-      .counter("foo", Map.empty, 23)
+      .counter("foo", Map("tag" -> "mytag"), 23)
       .build()
 
-    //val serverExpectation = put("/mything").willReturn(aResponse().withStatus(200))
-
     reporter.reportPeriodSnapshot(snapshot)
-    Thread.sleep(2000)
+    Thread.sleep(500)
 
     stub.verify(
       postRequestedFor(urlEqualTo("/"))
         .withRequestBody(cloudWatchBody(
-          "Action"                 -> "PutMetricData",
-          "Namespace"                      -> "kamon-cloudwatch",
+          "Action"                         -> "PutMetricData",
+          "Namespace"                      -> "kamon-cloudwatch-test",
           "MetricData.member.1.MetricName" -> "foo",
           "MetricData.member.1.Value"      -> "23.0",
           "MetricData.member.1.Unit"       -> "Count"
@@ -79,7 +68,8 @@ class CloudWatchReporterSpec extends FlatSpec with Matchers {
 
     val reporter: CloudWatchReporter = {
       val endpoint = ConfigFactory.parseMap(Map(
-        "kamon.cloudwatch.service-endpoint" -> s"http://localhost:${cloudWatch.port()}"
+        "kamon.cloudwatch.service-endpoint" -> s"http://localhost:${cloudWatch.port()}",
+        "kamon.cloudwatch.region"           -> "us-west-1"
       ).asJava)
 
       val r = new CloudWatchReporter(TestClock)
@@ -93,6 +83,21 @@ class CloudWatchReporterSpec extends FlatSpec with Matchers {
     new CloudWatchBodyPattern(Map(keys: _*))
 
   class CloudWatchBodyPattern(params: Map[String, String])
-    extends RegexPattern(params.map { case (k,v) => s"$k=$v"}.mkString("(.*?)", "(.*?)", "(.*?)"))
+    extends StringValuePattern(params.map { case (k,v) => s"$k=$v"}.mkString("&")) {
+
+      override def `match`(body: String): MatchResult = {
+        def keyValue(expr: String): Option[(String, String)] = {
+          val pairs = expr.split("=")
+          if (pairs.size != 2) None
+          else Some(pairs(0) -> pairs(1))
+        }
+        val keyValuePairs = body.split("&").flatMap(keyValue).toMap
+
+        println(keyValuePairs)
+
+        MatchResult.of(params.forall { case (k,v) => keyValuePairs.get(k).contains(v) })
+      }
+
+    }
 
 }
