3730fee38af75769d72c6378453b0f964121c8df
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-08-20 10:35:56 +0100

    Exaggerate waiting time to see if it makes any difference

--- src/test/scala/kamon/cloudwatch/CloudWatchReporterSpec.scala
+++ src/test/scala/kamon/cloudwatch/CloudWatchReporterSpec.scala
@@ -44,7 +44,7 @@ class CloudWatchReporterSpec extends FlatSpec with Matchers {
       .build()
 
     reporter.reportPeriodSnapshot(snapshot)
-    Thread.sleep(2500)
+    Thread.sleep(10500)
 
     stub.verify(
       postRequestedFor(urlEqualTo("/"))
