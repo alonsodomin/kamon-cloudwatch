106b0e8632b969997affd3fc85909949e72d0d21
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-10-31 20:46:37 +0100

    Update slf4j-api to 1.7.29 (#30)

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,6 +3,6 @@ object Versions {
   val log4j     = "2.12.1"
   val kamon     = "2.0.1"
   val scalatest = "3.0.8"
-  val slf4j     = "1.7.28"
+  val slf4j     = "1.7.29"
   val wiremock  = "2.25.1"
 }
