1cf8c448e2a5d4c8a993a15ddf476f157e016509
Scala Steward <me@scala-steward.org>
2019-12-15 17:12:59 +0100

    Update log4j-api, log4j-core, ... to 2.13.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val aws       = "1.11.693"
-  val log4j     = "2.12.1"
+  val log4j     = "2.13.0"
   val kamon     = "2.0.3"
   val scalatest = "3.1.0"
   val slf4j     = "1.7.29"
