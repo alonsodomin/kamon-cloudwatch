40788e5814176154174e3f8b6b45b20f736b797a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-05-10 12:34:04 +0200

    Update scalatest to 3.1.2 (#181)

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val aws       = "1.11.779"
   val log4j     = "2.13.2"
   val kamon     = "2.1.0"
-  val scalatest = "3.1.1"
+  val scalatest = "3.1.2"
   val slf4j     = "1.7.30"
   val wiremock  = "2.25.1"
 }
