cdb10352c99b82d88e9b4d263c78735b921f7757
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-22 20:35:54 +0100

    Update kamon-core, kamon-testkit to 2.1.13 (#437)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val aws       = "1.11.979"
   val log4j     = "2.14.1"
-  val kamon     = "2.1.12"
+  val kamon     = "2.1.13"
   val scalatest = "3.2.6"
   val slf4j     = "1.7.30"
   val wiremock  = "2.25.1"
