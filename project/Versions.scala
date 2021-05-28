0201bfbc9fc54d1f3696e7730de3fe0a451d2c2e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-28 18:18:25 +0200

    Update kamon-core, kamon-testkit to 2.1.20 (#498)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val aws       = "1.11.1029"
   val log4j     = "2.14.1"
-  val kamon     = "2.1.19"
+  val kamon     = "2.1.20"
   val scalatest = "3.2.9"
   val slf4j     = "1.7.30"
   val wiremock  = "2.25.1"
